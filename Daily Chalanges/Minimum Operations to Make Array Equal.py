77fdc79ea9072c45f85853d7aeff656b5e15d790
Sinkumen <sinkumen@gmail.com>
2021-04-22 12:12:42 +0300

    progress

--- Daily Chalanges/Minimum Operations to Make Array Equal.py
+++ Daily Chalanges/Minimum Operations to Make Array Equal.py
@@ -1,9 +1,12 @@
+import math
+
+
 class Solution:
     def minOperations(self, n: int) -> int:
         operations = 0
         first = 1
         last = (n-1)*2 + 1
         req = (first + last)//2
-        for i in range(ceil(n/2)):
+        for i in range(math.ceil(n/2)):
             operations += (req - (i*2 + 1))
         return operations
