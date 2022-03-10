74d44b9de4f3d5e28d7b7e6f2ae8ec999e7a664c
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-03-10 20:20:42 +0300

    Time: 40 ms (93.51%), Space: 18 MB (26.71%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,32 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        def dp(idx,buy,mem):
-            if (idx,buy) in mem:
-                return mem[(idx,buy)]
+        @lru_cache()
+        def dp(idx,buy):
             if idx >= len(prices):
                 return 0
+            
             if buy:
                 profit = 0
-                for i in range(idx+1,len(prices)):
-                    cur = prices[idx]
-                    nxt = prices[i]
-                    diff = prices[i] - prices[idx]
-                    if nxt > cur:
-                        profit = max(profit,diff + dp(i,False,mem))
-                mem[(idx,buy)] = profit
+                profit = max(profit,dp(idx+1,False)-prices[idx])
+                profit = max(profit,dp(idx+1,True))
                 return profit
             else:
                 profit = 0
-                for i in range(idx+2,len(prices)):
-                    profit = max(profit, dp(i,True,mem))
-                mem[(idx,buy)] = profit
+                profit = max(profit,dp(idx+2,True)+prices[idx])
+                profit = max(profit,dp(idx+1,False))
                 return profit
-        ans = 0
-        mem = {}
-        for i in range(len(prices)):
-            ans = max(ans,dp(i,True,mem))
-        return ans
+
+        return dp(0,True)
                     
                     
                 
