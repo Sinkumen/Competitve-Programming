26aa5486b5fe8934fcc6e04918f1c67a9a7ad2f7
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-03-19 12:30:42 +0300

    Time: 56 ms (27.86%), Space: 13.9 MB (76.43%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
@@ -1,29 +1,16 @@
 class Solution:
     def smallestSubsequence(self, s: str) -> str:
-        last = [-1]*26
-        for i in range(len(s)-1,-1,-1):
-            c = s[i]
-            if last[ord(c)-ord("a")] == -1:
-                last[ord(c)-ord("a")] = i
-                
+        d = Counter(s)
         visited = set()
-        
         stack = []
-        
-        
         for i in range(len(s)):
-            c = s[i]
-            
-            if c in visited:
-                continue
-            while stack and stack[-1][0] >= c:
-                top = stack[-1]
-                idx = ord(top) - ord("a")
-                if i < last[idx]:
+            d[s[i]] -= 1
+            if s[i] not in visited:
+                while stack and stack[-1] > s[i] and d[stack[-1]]:
+                    # stack.pop()
+                    visited.remove(stack[-1])
                     stack.pop()
-                    visited.remove(top)
-                else:
-                    break
-            stack.append(c)
-            visited.add(c)
+                visited.add(s[i])
+                stack.append(s[i])
+            
         return "".join(stack)
