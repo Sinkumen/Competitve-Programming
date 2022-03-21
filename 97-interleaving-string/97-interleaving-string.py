7d71c180c21237e6d8ab6e25f87f3600697895c5
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-03-21 20:57:13 +0300

    Time: 43 ms (87.00%), Space: 15.2 MB (23.37%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        l1 = len(s1)
+        l2 = len(s2)
+        l3 = len(s3)
+        if l1 + l2 != l3:
+            return False
+        @lru_cache(None)
+        def helper(p1,p2,p3):
+            if p3 == l3:
+                return True
+            if p1 < l1 and p2 < l2 and s1[p1] == s2[p2] == s3[p3]:
+                return helper(p1+1,p2,p3+1) or helper(p1,p2+1,p3+1)
+            elif p1 < l1 and s1[p1] == s3[p3]:
+                return helper(p1+1,p2,p3+1)
+            elif p2 < l2 and s2[p2] == s3[p3]:
+                return helper(p1,p2+1,p3+1)
+            else:
+                return False
+                
+        return helper(0,0,0)
+        
