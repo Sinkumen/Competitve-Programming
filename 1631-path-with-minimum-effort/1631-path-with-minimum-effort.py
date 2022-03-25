c56925c9f7cee123382ab6cae8bd28c6648ce6bc
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-03-25 13:27:20 +0300

    Time: 1952 ms (29.74%), Space: 23.9 MB (5.06%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        r = len(heights)-1
+        c = len(heights[0])-1
+        
+        def dfs(pos,k,visited):
+            if pos == (r,c):
+                return True
+            dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+            cur = heights[pos[0]][pos[1]]
+            for d in dirs:
+                nr = pos[0] + d[0]
+                nc = pos[1] + d[1]
+                if 0 <= nr <= r and 0 <= nc <= c:
+                    nxt = heights[nr][nc]
+                    if (nr,nc) not in visited and abs(nxt-cur) <= k:
+                        visited.add((nr,nc))
+                        res = dfs((nr,nc),k,visited)
+                        if res:
+                            return True
+            return False
+        start = 0
+        end = 10**6
+        ans = end
+        while start <= end:
+            mid = (start + end)//2
+            res = dfs((0,0),mid,set((0,0)))
+            if res:
+                ans = mid
+                end = mid - 1
+            else:
+                start = mid + 1
+        return ans
+                
+                
+                        
+                    
+            
