269eee971f7fbefefe31478bc38eb3f3ddfb73cb
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-03-25 16:32:27 +0300

    Time: 588 ms (96.40%), Space: 16.7 MB (88.88%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,27 +1,23 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        ans = [[ 10000 for j in range(len(mat[0]))] for i in range(len(mat))]
-        visited = set()
-        queue = deque()
-        dirs = [(0,1),(1,0),(-1,0),(0,-1)]
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if mat[i][j] == 0:
-                    ans[i][j] = 0
-                    queue.append((i,j,0))     
-        while queue:
-            x,y,level = queue.popleft()
-            if mat[x][y] == 1:
-                visited.add((x,y))
-                ans[x][y] = min(ans[x][y],level)
-
-            for d in dirs:
-                nx = x+d[0]
-                ny = y+d[1]
-                if 0 <= nx < len(mat) and 0 <= ny < len(mat[0]):
-                    if (nx,ny) not in visited and mat[nx][ny] == 1:
-                        visited.add((nx,ny))
-                        queue.append((nx,ny,level+1))
-                            
-        return ans
+                if mat[i][j] != 0:
+                    val = 10000
+                    if i > 0:
+                        val = min(val,mat[i-1][j])
+                    if j > 0:
+                        val = min(val,mat[i][j-1])
+                    mat[i][j] = val + 1
+             
+        for i in range(len(mat)-1,-1,-1):
+            for j in range(len(mat[0])-1,-1,-1):
+                if mat[i][j] != 0:
+                    val = 10000
+                    if i < len(mat)-1:
+                        val = min(val,mat[i+1][j])
+                    if j < len(mat[0])-1:
+                        val = min(val,mat[i][j+1])
+                    mat[i][j] = min(mat[i][j],val + 1)  
+        return mat
         
