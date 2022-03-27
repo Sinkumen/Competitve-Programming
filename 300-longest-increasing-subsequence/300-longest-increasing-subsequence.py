30d22b2a7752e5005ff91dcb193eec0b2ba94fd7
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-03-27 17:10:33 +0300

    Time: 4128 ms (50.10%), Space: 14.2 MB (87.05%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,7 +1,6 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [0]*len(nums)
-        dp[-1] = 1
+        dp = [1]*len(nums)
         for i in range(len(nums)-2,-1,-1):
             maxx = 0
             for j in range(i,len(nums)):
