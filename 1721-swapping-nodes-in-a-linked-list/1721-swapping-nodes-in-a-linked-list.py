6b572633419a066cc965ac9970c83bf957522986
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-04-11 20:00:22 +0300

    Time: 2199 ms (8.17%), Space: 145.6 MB (5.16%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        def findSize(node):
+            if not node:
+                return 0
+            return findSize(node.next) + 1
+        size = findSize(head)
+        first = head
+        last = head
+        cur = head
+        curSize = 1
+        
+        while cur:
+            if curSize < k:
+                first = cur.next
+            if size - curSize + 1 == k:
+                last = cur
+            cur = cur.next
+            curSize += 1
+        first.val, last.val = last.val, first.val
+        return head
+            
+        
