52e927f6da004a0c61519f40dbc58d9df87714d5
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-05-19 10:38:57 +0300

    Time: 452 ms (88.13%), Space: 19.6 MB (16.59%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,27 +1,22 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        def dfs(x,y,visited,mem):
-            if (x,y) in mem:
-                return mem[(x,y)]
+        @lru_cache(None)
+        def dfs(x,y):
             dirs = [(0,1),(1,0),(-1,0),(0,-1)]
             maxx = 0
             for d in dirs:
                 nx = x + d[0]
                 ny = y + d[1]
                 if 0 <= nx < n and 0 <= ny < m:
-                    if matrix[nx][ny] > matrix[x][y] and (nx,ny) not in visited:
-                        visited.add((nx,ny))
-                        maxx = max(maxx,dfs(nx,ny,visited,mem))
-                        visited.remove((nx,ny))
-            mem[(x,y)] = maxx + 1
-            return mem[(x,y)]
+                    if matrix[nx][ny] > matrix[x][y]:
+                        maxx = max(maxx,dfs(nx,ny))       
+            return maxx + 1
         
         n = len(matrix)
         m = len(matrix[0])
         ans = 1  
-        mem = {}
         for i in range(n):
             for j in range(m):
-                ans = max(ans,dfs(i,j,set(),mem))
+                ans = max(ans,dfs(i,j))
         return ans
         
