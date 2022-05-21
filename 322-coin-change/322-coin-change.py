d89889e2b7d61dca7834de9787a3d659d6c5fcc9
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-05-21 08:18:02 +0300

    Time: 845 ms (95.89%), Space: 15 MB (30.26%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,22 +1,35 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @lru_cache(None)
-        def dfs(cur):
+        queue = deque([(0,0)])
+        visited = set([0])
+        while queue:
+            cur, level = queue.popleft()
             if cur == amount:
-                return 0
-            if cur > amount:
-                return -1
-            ans = float("inf") 
+                return level
             for i in range(len(coins)):
-                res = dfs(cur+coins[i])
-                if res != -1:
-                    ans = min(ans,res)
+                nxt = coins[i] + cur
+                if nxt <= amount and nxt not in visited:
+                    visited.add(nxt)
+                    queue.append((nxt,level+1))
+        return -1
+    
+#         @lru_cache(None)
+#         def dfs(cur):
+#             if cur == amount:
+#                 return 0
+#             if cur > amount:
+#                 return -1
+#             ans = float("inf") 
+#             for i in range(len(coins)):
+#                 res = dfs(cur+coins[i])
+#                 if res != -1:
+#                     ans = min(ans,res)
                
-            return -1 if ans == float("inf") else ans + 1
-                
-           
+#             return -1 if ans == float("inf") else ans + 1
         
-        return dfs(0)
+#         return dfs(0)
+
+    
                 
             
             
