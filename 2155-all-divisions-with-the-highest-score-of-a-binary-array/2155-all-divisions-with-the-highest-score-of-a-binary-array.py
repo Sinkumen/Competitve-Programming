3fc67287c649408adce2fe760ce946344ddd11a4
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-05-23 11:08:48 +0300

    Time: 4260 ms (60.31%), Space: 46.3 MB (7.22%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -8,18 +8,17 @@ class Solution:
             pre.append(zeros)
         suff = [0]*len(nums)
         ones = 0
+        mapping = defaultdict(list)
         maxx = pre[-1]
+        mapping[pre[-1]].append(len(pre)-1)
         for i in range(len(nums)-1,-1,-1):
             if nums[i] == 1:
                 ones += 1
             suff[i] = ones
             maxx  = max(maxx,ones+pre[i])
-        suff.append(0)
-        ans = []
-        for j in range(len(pre)):
-            if pre[j] + suff[j] == maxx:
-                ans.append(j)
-        return ans
+            mapping[ones+pre[i]].append(i)
+        
+        return mapping[maxx]
             
             
             
