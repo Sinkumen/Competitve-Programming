1c0a826f25e5b66c464312c3b8e13566342586dc
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-03 10:23:20 +0300

    Time: 1982 ms (58.34%), Space: 25 MB (40.31%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -1,27 +1,18 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.matrix = matrix.copy()
+        self.matrix = [[0 for _ in range(len(matrix[0])+1)] for _ in range(len(matrix)+1)]
         for i in range(len(matrix)):
             for j in range(len(matrix[0])): 
-                if i-1 >= 0 and j-1 >= 0:
-                    self.matrix[i][j] += matrix[i-1][j] + matrix[i][j-1]
-                    self.matrix[i][j] -= matrix[i-1][j-1] 
-                elif i-1 >= 0:
-                    self.matrix[i][j] += matrix[i-1][j]
-                elif j-1 >= 0:
-                    self.matrix[i][j] += matrix[i][j-1]
-
+                self.matrix[i+1][j+1] = matrix[i][j]
+        
+        for i in range(1,len(self.matrix)):
+            for j in range(1,len(self.matrix[0])):
+                self.matrix[i][j] += self.matrix[i-1][j] + self.matrix[i][j-1] - self.matrix[i-1][j-1] 
+                
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        ans = self.matrix[row2][col2]
-        if row1-1 >= 0 and col1-1 >= 0:
-            ans -= self.matrix[row1 - 1][col2] + self.matrix[row2][col1-1]
-            ans += self.matrix[row1 - 1][col1-1]
-        elif row1-1 >= 0:
-            ans -= self.matrix[row1-1][col2]
-        elif col1-1 >= 0:
-            ans -= self.matrix[row2][col1-1]
-        return ans
+        return self.matrix[row2+1][col2+1] - self.matrix[row1][col2+1] - self.matrix[row2+1][col1] + self.matrix[row1][col1]
+        
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
