f06be6b8574d2cfd357b0fb13abbc61f9ae32018
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-05 08:24:26 +0300

    Time: 180 ms (19.56%), Space: 14.3 MB (84.07%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,7 +1,7 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         board = [[0 for _ in range(n)] for _ in range(n)]
-        possible = set()
+        possible = []
         def disablePath(x,y,val):
             board[x][y] += val
             for i in range(1,n+1):
@@ -13,30 +13,23 @@ class Solution:
                 if y-i >= 0 and x-i >= 0: board[x-i][y-i] += val
                 if y-i >= 0 and x+i < n: board[x+i][y-i] += val
                 if y+i < n and x-i >= 0: board[x-i][y+i] += val
-       
+        ans = []
         def dfs(path,r):
             if len(path) == n:
-                possible.add(tuple(sorted(path)))
+                ans.append(tuple(path))
                 return
             if r < n:
                 for j in range(n):
                     if board[r][j] == 0:
+                        row = ["." for i in range(n)]
+                        row[j] = "Q"
                         disablePath(r,j,-1)
-                        path.append((r,j))
+                        path.append("".join(row))
                         dfs(path,r+1)
                         path.pop()
                         disablePath(r,j,1)
         dfs([],0)
-        ans = []
-        # print(possible)
-        for poss in possible:
-            final = [["." for _ in range(n)] for _ in range(n)]
-            res = []
-            for x,y in poss:
-                final[x][y] = "Q"
-            for row in final:
-                res.append("".join(row))
-            ans.append(res)
+        
         return ans
                         
                     
