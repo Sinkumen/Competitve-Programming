9e62464a8210aea4f22bff8973f071c801244de7
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-05 08:14:55 +0300

    Time: 299 ms (9.30%), Space: 14.7 MB (6.43%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,28 +1,43 @@
 class Solution:
-    def solveNQueens(self, n):
-        res = []
-        self.dfs([-1]*n, 0, [], res)
-        return res
- 
-    # nums is a one-dimension array, like [1, 3, 0, 2] means
-    # first queen is placed in column 1, second queen is placed
-    # in column 3, etc.
-    def dfs(self, nums, index, path, res):
-        if index == len(nums):
-            res.append(path)
-            return  # backtracking
-        for i in range(len(nums)):
-            nums[index] = i
-            if self.valid(nums, index):  # pruning
-                tmp = "."*len(nums)
-                self.dfs(nums, index+1, path+[tmp[:i]+"Q"+tmp[i+1:]], res)
-
-    # check whether nth queen can be placed in that column
-    def valid(self, nums, n):
-        for i in range(n):
-            if abs(nums[i]-nums[n]) == n -i or nums[i] == nums[n]:
-                return False
-        return True
-
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [[0 for _ in range(n)] for _ in range(n)]
+        possible = set()
+        def disablePath(x,y,val):
+            board[x][y] += val
+            for i in range(1,n+1):
+                if y+i < n: board[x][y+i] += val
+                if y-i >= 0: board[x][y-i] += val
+                if x+i < n: board[x+i][y] += val
+                if x-i >= 0: board[x-i][y] += val
+                if y+i < n and x+i < n: board[x+i][y+i] += val
+                if y-i >= 0 and x-i >= 0: board[x-i][y-i] += val
+                if y-i >= 0 and x+i < n: board[x+i][y-i] += val
+                if y+i < n and x-i >= 0: board[x-i][y+i] += val
+       
+        def dfs(path,r):
+            if len(path) == n:
+                possible.add(tuple(sorted(path)))
+                return
+            if r < n:
+                for j in range(n):
+                    if board[r][j] == 0:
+                        disablePath(r,j,-1)
+                        path.append((r,j))
+                        dfs(path,r+1)
+                        path.pop()
+                        disablePath(r,j,1)
+        dfs([],0)
+        ans = []
+        # print(possible)
+        for poss in possible:
+            final = [["." for _ in range(n)] for _ in range(n)]
+            res = []
+            for x,y in poss:
+                final[x][y] = "Q"
+            for row in final:
+                res.append("".join(row))
+            ans.append(res)
+        return ans
+                        
                     
                     
