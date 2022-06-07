6440add36c1eb7c17eac1a7c3a6c7cef0ef4972d
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-07 17:57:37 +0300

    Time: 1328 ms (26.30%), Space: 20.6 MB (88.82%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,6 +1,6 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        prevCount = defaultdict(int)
+        prevCount = [0]*(len(nums)+1)
         prevCount[0] = 1
         res = 0
         count = 0
@@ -8,6 +8,6 @@ class Solution:
             if num % 2 == 1:
                 count += 1
             prevCount[count] += 1
-            if count - k in prevCount:
+            if prevCount[count - k]:
                 res += prevCount[count - k]
         return res
