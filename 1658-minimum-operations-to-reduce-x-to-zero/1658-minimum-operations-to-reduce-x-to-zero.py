4c13af46aa5ce20b35feb3b48de571541931cb9d
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-11 09:00:44 +0300

    Time: 3751 ms (5.20%), Space: 28.3 MB (44.81%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,6 +1,6 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        pre = []
+        pre = [0]
         s = 0
         for i in range(len(nums)):
             s += nums[i]
@@ -10,8 +10,9 @@ class Solution:
         for i in range(len(nums)-1,-1,-1):
             s += nums[i]
             suf[i] = s
+        suf.append(0)
             
-        def search(n,arr,pre):
+        def search(n,arr):
             left = 0
             right = len(arr)-1
             while left <= right:
@@ -19,32 +20,26 @@ class Solution:
                 if arr[mid] == n:
                     return mid
                 elif arr[mid] > n:
-                    if pre:
-                        right = mid -1 
-                    else:
-                        left = mid + 1
+                    left = mid + 1
                 else:
-                    if pre:
-                        left = mid + 1     
-                    else:
-                        right = mid - 1 
+                    right = mid - 1 
             return -1
 
-        def binary(arr1,arr2,pre):
+        def binary(arr1,arr2):
             ans = float("inf")
             for i in range(len(nums)):
                 if arr1[i] == x:
-                    ans = min(ans,i+1) if pre else min(ans,len(nums)-i)
+                    ans = min(ans,i) 
                     continue
                 if arr1[i] > x:
                     continue
-                if (pre and arr1[-1] > x) or (not pre and arr1[0] > x):
-                    res = search(x - arr1[i],arr2,not pre)
+                
+                if arr1[-1] >= x:
+                    res = search(x - arr1[i],arr2)
                     if res != -1:
-                        step = (i + 1 + (len(nums)-res)) if pre else (res + 1 + (len(nums)-i))
+                        step = (i + (len(nums)-res)) if pre else (res + (len(nums)-i))
                         ans = min(step,ans)
             return ans
-        
-        temp = min(binary(pre,suf,True),binary(suf,pre,False))
+        temp = binary(pre,suf)
         return temp if temp < float("inf") else -1
                         
