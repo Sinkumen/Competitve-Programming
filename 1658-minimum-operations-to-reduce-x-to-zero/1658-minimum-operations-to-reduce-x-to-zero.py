260de8de29d7c8128f5e5428f9d16747bc2c0c29
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-11 09:13:26 +0300

    Time: 5769 ms (5.20%), Space: 28.3 MB (44.81%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,43 +1,37 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
+        def search(n):
+            left = 0
+            right = len(pre)-1
+            while left <= right:
+                mid = (left + right)//2
+                if pre[mid] == n:
+                    return mid
+                elif pre[mid] > n:
+                    right = mid - 1 
+                else:
+                    left = mid + 1        
+            return -1
+        
         pre = [0]
         s = 0
         for i in range(len(nums)):
             s += nums[i]
             pre.append(s)
-        suf = [0]*len(nums)
+        if pre[-1] < x:
+            return -1
+        
+        ans = float("inf")
+        nums.append(0)
         s = 0
         for i in range(len(nums)-1,-1,-1):
             s += nums[i]
-            suf[i] = s
-        suf.append(0)
+            res = search(x - s)
+            if res != -1:
+                step = res + (len(nums)-i-1)
+                ans = min(step,ans)
             
-        def search(n,arr):
-            left = 0
-            right = len(arr)-1
-            while left <= right:
-                mid = (left + right)//2
-                if arr[mid] == n:
-                    return mid
-                elif arr[mid] > n:
-                    left = mid + 1
-                else:
-                    right = mid - 1 
-            return -1
 
-        ans = float("inf")
-        for i in range(len(nums)):
-            if pre[i] == x:
-                ans = min(ans,i) 
-                continue
-            if pre[i] > x:
-                continue
-
-            if pre[-1] >= x:
-                res = search(x - pre[i],suf)
-                if res != -1:
-                    step = (i + (len(nums)-res)) if pre else (res + (len(nums)-i))
-                    ans = min(step,ans)
         return ans if ans < float("inf") else -1
       
                         
