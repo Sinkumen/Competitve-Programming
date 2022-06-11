d949916ed1d274e7facd0decb34a073278c9bcad
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-11 09:41:56 +0300

    Time: 1806 ms (34.92%), Space: 28 MB (62.03%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,36 +1,24 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        def search(n):
-            left = 0
-            right = len(pre)-1
-            while left <= right:
-                mid = (left + right)//2
-                if pre[mid] == n:
-                    return mid
-                elif pre[mid] > n:
-                    right = mid - 1 
-                else:
-                    left = mid + 1        
-            return -1
-        
-        pre = [0]
-        s = 0
-        for i in range(len(nums)):
-            s += nums[i]
-            pre.append(s)
-        if pre[-1] < x:
-            return -1
-        
-        ans = float("inf")
-        nums.append(0)
-        s = 0
-        for i in range(len(nums)-1,-1,-1):
-            s += nums[i]
-            res = search(x - s)
-            if res != -1:
-                step = res + (len(nums)-i-1)
-                ans = min(step,ans)
+        total = sum(nums)
+        window = total - x
+        if window < 0:
+            return - 1
+        if not window:
+            return len(nums)
+        left,right = 0,0
+        ans = -1
+        cur = 0
+        while right < len(nums):
+            cur += nums[right]
+            while cur >= window:
+                if cur== window:
+                    ans= max(ans,right-left+1)
+                cur -= nums[left]
+                left += 1
+            right += 1
             
-        return ans if ans < float("inf") else -1
+        return len(nums) - ans if ans >= 0 else -1
+        
       
                         
