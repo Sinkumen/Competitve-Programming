0c5ddd5830e939b208ee6a69afe0c635a995bbea
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-16 09:46:41 +0300

    Time: 1579 ms (43.57%), Space: 14 MB (59.59%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,29 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        width = 1
-        l = 0
-        r = 0
-        for i in range(len(s)):
-            left = i 
-            right = i
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                if (right - left + 1) > width:
-                    l = left
-                    r = right
-                    width = (right - left + 1)
-                left -= 1
-                right += 1
-            left = i 
-            right = i+1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                if right - left + 1 > width:
-                    l = left
-                    r = right
-                    width = right - left + 1
-                left -= 1
-                right += 1
-        return s[l:r+1]
-            
-                    
+        def expand(l,r):
+            length = 0
+            left,right = l,r
+            while 0 <= l<= r < len(s) and s[l] == s[r]:
+                left = l
+                right = r
+                if l == r:
+                    length += 1
+                else:
+                    length += 2
+                l -= 1
+                r += 1
+            return (length,left,right)
+        
+        left,right = 0,0
+        max_ = 0
+        for i in range(1,len(s)):
+            one = expand(i,i)
+            two = expand(i-1,i)
+            if one[0] > two[0] and one[0] > max_:
+                max_,left,right = one[0],one[1],one[2]
+            if one[0] <= two[0] and two[0] > max_:
+                max_,left,right = two[0],two[1],two[2]
                 
-                    
+        return s[left:right+1]
+
+                
+            
