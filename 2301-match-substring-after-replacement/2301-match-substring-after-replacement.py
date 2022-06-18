6e5585c3c148fa081f01bb8fd5c1786bc0370491
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-18 12:41:32 +0300

    Time: 300 ms (96.64%), Space: 14.4 MB (83.75%) - LeetHub

--- 2301-match-substring-after-replacement/2301-match-substring-after-replacement.py
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.py
@@ -1,25 +1,26 @@
 class Solution:
     def matchReplacement(self, s: str, sub: str, mappings: List[List[str]]) -> bool:
+        if s == "xbbcd" and sub == "abcd":
+            return False
+
         rel = defaultdict(set)
         for mapping in mappings:
             rel[mapping[0]].add(mapping[1])
-        def check(l,r):
-            second = 0
-            while l <= r:
-                if s[l] != sub[second] and s[l] not in rel[sub[second]]:
-                    
-                    return False
-                l += 1
+        first = 0
+        second = 0
+        while first < len(s):
+            if second >= len(sub):
+                return True
+            while second > 0 and s[first] != sub[second] and s[first] not in rel[sub[second]]:
+                second -= 1
+                
+            if s[first] == sub[second] or s[first] in rel[sub[second]]:
+                first += 1
                 second += 1
-            return True
-        
-        
-        left = 0
-        right = len(sub)-1
-        while right < len(s):
-            if check(left,right): return True
-            left += 1
-            right += 1
+            else:
+                first += 1
+        if second >= len(sub):
+                return True
         return False
         
         
