23b4d917d2245187c4d119703291a7eee35f560d
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-21 14:48:48 +0300

    Time: 47 ms (40.15%), Space: 13.9 MB (20.32%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,29 +1,23 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        word = []
-        i = 0
-        while i < len(s):
-            if s[i].isalpha():
-                word.append(s[i])
-            elif s[i] == "[":
-                cur = i+1
-                count = 0
-                while count or s[cur] != "]":
-                    if s[cur] == "[":
-                        count += 1
-                    elif s[cur] == "]":
-                        count -= 1
-                    cur += 1
-                res = self.decodeString(s[i+1:cur])
-                temp = cur+1
-                back = i-1
+        stack = []
+        for i in range(len(s)):
+            temp = []
+            if s[i] == "]":
+                while stack[-1] != "[":
+                    temp.append(stack.pop())
+                stack.pop()
                 multi = ""
-                while back >= 0 and s[back].isdigit():
-                        multi = s[back] + multi
-                        back -= 1
-                word.append(res*int(multi))
-                i = temp
-                continue
-            i += 1
-        return "".join(word)
+                while stack and stack[-1].isdigit():
+                    top = stack.pop()
+                    multi = top + multi
+                rep = "".join(temp[::-1])*int(multi)
+                for c in rep:
+                    stack.append(c)
+            else:
+                stack.append(s[i])
+        return "".join(stack)
+                
+            
+            
             
