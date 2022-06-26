c59c826f9f7a36b497ceaceec1d2045d703d6f98
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-26 19:24:45 +0300

    Time: 54 ms (77.78%), Space: 14.6 MB (40.05%) - LeetHub

---
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minDays(self, n: int) -> int:
+        @lru_cache(None)
+        def fn(n):
+            if n <= 1: return n
+            return 1 + min(n%2 + fn(n//2), n%3 + fn(n//3))
+        
+        return fn(n)
