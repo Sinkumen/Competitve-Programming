bf1e35da5e95c128d477887ca7d53990f529eef1
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-06-26 19:50:30 +0300

    Time: 225 ms (13.18%), Space: 14.1 MB (76.74%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -1,8 +1,18 @@
 class Solution:
     def minDays(self, n: int) -> int:
-        @lru_cache(None)
-        def fn(n):
-            if n <= 1: return n
-            return 1 + min(n%2 + fn(n//2), n%3 + fn(n//3))
-        
-        return fn(n)
+        queue = deque([(n,0)])
+        visited = set()
+        while queue:
+            cur,level = queue.popleft()
+            if not cur:
+                return level
+            if cur-1 not in visited:
+                visited.add(cur-1)
+                queue.append((cur-1,level+1))
+            if cur%2==0 and cur//2 not in visited:
+                visited.add(cur//2)
+                queue.append((cur//2,level+1))
+            if cur%3==0 and ((cur//3)) not in visited:
+                visited.add((cur//3))
+                queue.append(((cur//3),level+1))
+           
