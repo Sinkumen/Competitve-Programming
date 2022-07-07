a716c8cfc1c207f7cdb779b2cef6d40482f4fc19
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-07-07 07:11:54 +0300

    Time: 87 ms (21.71%), Space: 14.3 MB (53.36%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,23 +1,20 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        l1,l2,l3 = len(s1),len(s2),len(s3)
-
-        if l1 + l2 != l3:
+        @lru_cache()
+        def dfs(p1,p2,p3):
+            if p3 >= len(s3) and p1 >= len(s1) and p2 >= len(s2):
+                return True
+            
+            if p1 < len(s1) and p2 < len(s2) and p3 < len(s3) and s1[p1] == s2[p2] == s3[p3]:
+                one = dfs(p1+1,p2,p3+1)
+                two = dfs(p1,p2+1,p3+1)
+                return one or two
+            
+            if p1 < len(s1) and p3 < len(s3) and s1[p1] == s3[p3]:
+                return dfs(p1+1,p2,p3+1)
+            
+            if p2 < len(s2) and p3 < len(s3) and  s2[p2] == s3[p3]:
+                return dfs(p1,p2+1,p3+1)
             return False
         
-        @lru_cache(None)
-        def helper(p1,p2,p3):
-            
-            if p3 == l3:
-                return True
-            if p1 < l1 and p2 < l2 and s1[p1] == s2[p2] == s3[p3]:
-                return helper(p1+1,p2,p3+1) or helper(p1,p2+1,p3+1)
-            elif p1 < l1 and s1[p1] == s3[p3]:
-                return helper(p1+1,p2,p3+1)
-            elif p2 < l2 and s2[p2] == s3[p3]:
-                return helper(p1,p2+1,p3+1)
-            else:
-                return False
-                
-        return helper(0,0,0)
-        
+        return dfs(0,0,0)
