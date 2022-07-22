cbe7145949cfd60bf6efc959f09528e5cbf9620a
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-07-22 14:50:30 +0300

    Time: 9805 ms (5.02%), Space: 14 MB (50.90%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,27 +1,24 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        z = len(board[0])
-        n = len(word)
-        def is_valid(i,j)->bool:
-            return 0 <= i < m and 0 <= j < z
-        def dfs(i, j, k):
-            if k == n or (k+1 == n and word[k] == board[i][j] and board[i][j]):
-                return True
-            DIR = (1, 0, -1, 0, 1)
-            for ii in range(4):
-                r, c = i+DIR[ii], j+DIR[ii+1]
-                if is_valid(r,c) and board[r][c] == word[k] and board[r][c]:
-                    temp, board[r][c] = board[r][c], 0
-                    if dfs(r, c, k+1):
+        def dfs(i,x,y):
+            if i == len(word)-1:
+                if board[x][y] == word[i]:
+                    return True
+                return False
+            direcs = [(0,1),(1,0),(-1,0),(0,-1)]
+            for direc in direcs:
+                nx = x + direc[0]
+                ny = y + direc[1]
+                if 0 <= nx < len(board) and 0 <= ny < len(board[0]):
+                    board[x][y] = "*"
+                    if board[nx][ny] == word[i+1] and dfs(i+1,nx,ny):
                         return True
-                    board[r][c] = temp
+                    board[x][y] = word[i]
+                    
             return False
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    temp, board[i][j] = board[i][j],0
-                    if dfs(i,j,1):
-                        return True
-                    board[i][j]=temp
+                if board[i][j] == word[0] and dfs(0,i,j):
+                    return True
         return False
+                    
