90d163470d6055c5398b10b1c91808624a9a7dec
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-07-22 14:38:27 +0300

    Time: 9466 ms (9.58%), Space: 14 MB (50.90%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,27 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        def dfs(i,x,y,visited):
+            if i == len(word)-1:
+                if board[x][y] == word[i]:
+                    return True
+                return False
+            direcs = [(0,1),(1,0),(-1,0),(0,-1)]
+            for direc in direcs:
+                nx = x + direc[0]
+                ny = y + direc[1]
+                if 0<= nx < len(board) and 0 <= ny < len(board[0]) and (nx,ny) not in visited:
+                    visited.add((nx,ny))
+                    if board[nx][ny] == word[i+1] and dfs(i+1,nx,ny,visited):
+                        return True
+                    visited.remove((nx,ny))
+            return False
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    res = self.dfs((i,j),0,board,word,set([(i,j)]))
-                    if res:
-                        return True
+                if board[i][j] == word[0] and dfs(0,i,j,set([(i,j)])):
+                    return True
         return False
-    def dfs(self,pos,level,board,word,visited):
-        if level == len(word)-1:
-            return True
-        x,y = pos
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
-        for d in directions:
-            newX = x+d[0]
-            newY = y+d[1]
-            if 0 <= newX < len(board) and 0 <= newY < len(board[0]):
-                if (board[newX][newY] == word[level+1]) and (newX,newY) not in visited:
-                    visited.add((newX,newY))
-                    res = self.dfs((newX,newY),level+1,board,word,visited)
-                    if res:
-                        return True
-                    visited.remove((newX,newY))
-        return False
+                    
