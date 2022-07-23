e6a2ff640cdcdf7484056827c7508ac265a965c0
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-07-23 07:48:41 +0300

    Time: 4416 ms (49.82%), Space: 43.1 MB (14.34%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,47 +1,33 @@
-from sortedcontainers import SortedList
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        arr = []
         ans = [0]*len(nums)
-        for i in range(len(nums)):
-            arr.append((nums[i],i))
         
         def merge(arr):
-            if len(arr)==1:
+            if len(arr) == 1:
                 return arr
-            mid = len(arr)//2
-            left = merge(arr[:mid])
-            right = merge(arr[mid:])
+            left = merge(arr[:len(arr)//2])
+            right = merge(arr[len(arr)//2:])
+            
             l = 0
             r = 0
+            les = 0
             res = []
-            smaller = 0
             while l < len(left) and r < len(right):
                 if left[l][0] > right[r][0]:
+                    les += 1
                     res.append(right[r])
-                    smaller += 1
                     r += 1
                 else:
-                    ans[left[l][1]] += smaller
+                    ans[left[l][1]] += les
                     res.append(left[l])
                     l += 1
-                    
             while l < len(left):
-                ans[left[l][1]] += smaller
+                ans[left[l][1]] += les
                 res.append(left[l])
                 l += 1
-                
-            while r < len(right):
-                res.append(right[r])
-                r += 1
+            res.extend(right[r:])
+            
             return res
-        temp = merge(arr)
-
+        merge([(nums[i],i) for i in range(len(nums))])
         return ans
-            
-        
-            
-                    
-            
-        
-        
+                
