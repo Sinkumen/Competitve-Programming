b870ac1df1bcafaf0720873305c9ef1746cdc7f3
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-07-24 08:15:56 +0300

    Time: 310 ms (27.70%), Space: 20.5 MB (40.22%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,20 +1,37 @@
 class Solution:
-    def searchMatrix(self, mat: List[List[int]], target: int) -> bool:
-        def searchRow(row):
-            start = 0
-            end = len(mat[row])-1
-            while start <= end:
-                mid = (start+end)//2
-                if mat[row][mid] == target:
-                    return True
-                elif mat[row][mid] < target:
-                    start = mid + 1
-                else:
-                    end = mid - 1
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        def binary(isrow,row):
+            left = 0
+            right = len(matrix[0])-1 if isrow else len(matrix)-1
+            if isrow:
+                while left <= right:
+                    mid = (left + right)//2
+                    if matrix[row][mid] == target:
+                        return True
+                    elif matrix[row][mid] > target:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                return False
+            else:
+                ans = -1
+                while left <= right:
+                    mid = (left + right)//2
+
+                    if matrix[mid][0] == target:
+                        return mid
+                    elif matrix[mid][0] > target:
+                        right = mid - 1
+                    else:
+                        ans = mid
+                        left = mid + 1
+                return ans
+        row = binary(False,0)
+        if row == -1:
             return False
-        for i in range(len(mat)):
-            res = searchRow(i)
-            if res:
+        for i in range(row+1):
+            if binary(True,i):
                 return True
         return False
-       
+                
+                
