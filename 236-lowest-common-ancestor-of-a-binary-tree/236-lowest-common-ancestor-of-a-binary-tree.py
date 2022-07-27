9b5e38b37dcdf562905a47eca884c6cdcc67f497
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-07-27 08:32:37 +0300

    Time: 66 ms (98.20%), Space: 26 MB (82.46%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,37 +6,19 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, node: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        return self.dfs(node,p,q)[0]
-    def dfs(self,node,p,q):
-        if not node.left and not node.right:
-            if node.val == q.val or node.val == p.val:
-                return (node,1)
-            else:
-                return (node,0)
-            
-        left = (0,0)
-        right = (0,0)
-        
-        if node.left:
-            left = self.dfs(node.left,p,q)
-        if node.right:
-            right =  self.dfs(node.right,p,q)
-        
-        if left[1] == 1 and right[1] == 1:
-            return (node,2)
-        
-        if left[1] == 2:
-            return left
-        if right[1] == 2:
-            return right
-        # print(node.val,left[0].val,right[0].val)
-        if node.val == q.val or node.val == p.val:
-            # print(node.val,left[0].val,right[0].val)
-            if left[1] == 1 or right[1] == 1:
-                return (node,2)
-            else:
-                return (node,1)
-        if left[1] == 1 or right[1] == 1:
-            return (node,1)
-        return (node,0)
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(node):
+            if not node:
+                return
+            if node.val == p.val or node.val == q.val:
+                return node
+            left = dfs(node.left)
+            right = dfs(node.right)
+            if left and right:
+                return node
+            if left:
+                return left
+            if right:
+                return right
+                
+        return dfs(root)
