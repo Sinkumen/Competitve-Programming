498c9f4a3537f15c27af14f417a32bbffcd85641
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-07-31 19:41:30 +0300

    Time: 1210 ms (100.00%), Space: 55.8 MB (20.00%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -0,0 +1,46 @@
+class Solution:
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        one = set()
+        idx1 = defaultdict(int)
+        idx2 = defaultdict(int)
+        two = set()
+        visited1 = set()
+        visited2 = set()
+        count = 0
+        while node1 != -1:
+            if node1 not in visited1:
+                one.add(node1)
+                idx1[node1] = count
+                visited1.add(node1)
+                node1 = edges[node1]
+                count += 1
+            else:
+                node1 = -1
+        count = 0
+        while node2 != -1:
+            if node2 not in visited2:
+                two.add(node2)
+                idx2[node2] = count
+                visited2.add(node2)
+                node2 = edges[node2]
+                count += 1
+            else:
+                node2 = -1
+        # print(idx1)
+        # print(one)
+        # print(idx2)
+        # print(two)
+       
+        ans = -1
+        order = float("inf")
+        for node in one:
+            if node in two:
+                cur = max(idx1[node],idx2[node])
+                if order > cur:
+                    ans = node
+                    order = cur
+                elif order == cur and node < ans:
+                    ans = node    
+        return ans
+       
+               
