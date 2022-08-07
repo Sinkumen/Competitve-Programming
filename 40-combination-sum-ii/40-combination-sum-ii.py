4b37df1d5eb353af96e7ea07a977bb784c4661b5
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-08-07 12:22:50 +0300

    Time: 63 ms (91.13%), Space: 14.1 MB (5.59%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -8,17 +8,17 @@ class Solution:
             if summ == target:
                 ans.add(tuple(res))
                 return
+            visited = set()
             for j in range(i,len(candidates)):
-                
-                if j > i and candidates[j] == candidates[j-1]:
-                    continue
                 n = candidates[j]
-                if summ + n <= target:
-                    res.append(n)
-                    helper(j+1,summ+n,res)
-                    res.pop()
-                else:
-                    break
+                if n not in visited:
+                    visited.add(n)
+                    if summ + n <= target:
+                        res.append(n)
+                        helper(j+1,summ+n,res)
+                        res.pop()
+                    else:
+                        break
         if total >= target:
             helper(0,0,[])  
         return ans
