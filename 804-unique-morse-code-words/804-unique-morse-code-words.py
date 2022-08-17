2ee63e524ba69d7696ada3e509b73dd1a1d23b44
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-08-17 20:16:04 +0300

    Time: 49 ms (66.32%), Space: 13.9 MB (75.48%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.py
+++ 804-unique-morse-code-words/804-unique-morse-code-words.py
@@ -1,10 +1,5 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
         morses = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        group = set()
-        for word in words:
-            morse = []
-            for char in word:
-                morse.append(morses[ord(char)-ord("a")])
-            group.add("".join(morse))
-        return len(group)
+ 
+        return len({"".join(morses[ord(char)-ord("a")] for char in word) for word in words})
