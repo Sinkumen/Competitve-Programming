39ff83feab8e5441ac5f50e591a3756ebd56ff42
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-08-26 06:21:19 +0300

    Time: 3762 ms (17.11%), Space: 13.8 MB (96.79%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -3,7 +3,7 @@ class Solution:
         def Log2(x):
             return (math.log10(x) / math.log10(2))
         
-        for perm in itertools.permutations([x for x in str(n)]):
+        for perm in itertools.permutations(str(n)):
             if perm[0] != "0" :
                 num = int("".join(perm))
                 if ceil(Log2(num)) == floor(Log2(num)):
