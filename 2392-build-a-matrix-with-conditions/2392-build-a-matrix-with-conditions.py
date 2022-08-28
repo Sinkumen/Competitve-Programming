488fd247017b23104c44501ec83ba96a42f2bb94
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-08-28 09:06:09 +0300

    Time: 923 ms (100.00%), Space: 23.7 MB (100.00%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -12,20 +12,20 @@ class Solution:
                 cvisited.add(rc)
                 rgraph[rc[0]].append(rc[1])
                 rindegree[rc[1]] += 1
+                
         for cc in colConditions:
             cc = tuple(cc)
             if cc not in rvisited:
                 rvisited.add(cc)
                 cgraph[cc[0]].append(cc[1])
                 cindegree[cc[1]] += 1
-        # print(rgraph)
-        # print(cindegree)
+
         def find(indegree,graph):
             queue = deque()
             for i in range(1,k+1):
                 if not indegree[i]:
                     queue.append(i)
-            # print("q",queue)
+
             rows = []
             while queue:
                 top = queue.popleft()
@@ -37,28 +37,23 @@ class Solution:
             if len(rows) == k:
                 return rows
             return []
-        # print(rindegree)
+
         row = find(rindegree,rgraph)
         col = find(cindegree,cgraph)
         ans = [[0 for _ in range(k)] for _ in range(k)]
-        # print(row,col,rgraph)
+
         if row and col:
             pos = defaultdict(list)
             for i in range(len(row)):
                 pos[row[i]].append(i)
             for i in range(len(col)):
                 pos[col[i]].append(i)
-            # print(pos)
-            # print(ans)
+
             for i in range(1,k+1):
                 ans[pos[i][0]][pos[i][1]] = i
             
             return ans
         return []
-    """
-[0,0,1],
-[0,3,0],
-[2,0,0]]
-    """
+
             
         
