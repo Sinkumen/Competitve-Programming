563f3da2960f466617890e7db5ecfbec1e3c8886
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-08-29 11:28:14 +0300

    Time: 464 ms (57.27%), Space: 16.6 MB (50.32%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,21 +1,18 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def dfs(i,j):
-            nonlocal visited
+            grid[i][j] = 0
             directions = [(0,1),(1,0),(-1,0),(0,-1)]
             for direc in directions:
                 nx = i + direc[0]
                 ny = j + direc[1]
                 if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
-                    if (nx,ny) not in visited and grid[nx][ny] == "1":
-                        visited.add((nx,ny))
+                    if grid[nx][ny] == "1":
                         dfs(nx,ny)
-        visited = set()
         ans = 0       
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    visited.add((i,j))
+                if grid[i][j] == "1":
                     dfs(i,j)
                     ans += 1
        
