e12e5c5f3179760555c4cd57b92cbe484aafcbaf
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-09-15 21:18:11 +0300

    Time: 53 ms (59.72%), Space: 14.3 MB (39.28%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -3,22 +3,16 @@ class Solution:
         ans = []
         def dfs(rem,closed,res):
             if not rem:
-                res.append(")"*(n-closed))
-                ans.append("".join(res))
-                res.pop()
+                ans.append("".join(res) + ")"*(n-closed))
                 return
+            res.append("(")
+            dfs(rem-1,closed,res)
+            res.pop()
             if n-rem > closed:
                 res.append(")")
                 dfs(rem,closed + 1,res)
                 res.pop()
-                
-                res.append("(")
-                dfs(rem-1,closed,res)
-                res.pop()
-            else:
-                res.append("(")
-                dfs(rem-1,closed,res)
-                res.pop()
+           
         dfs(n,0,[])
         return ans
                 
