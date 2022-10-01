beddbea4fb81a877bc3144dc647bec7dae90bd8b
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-10-01 18:23:11 +0300

    Time: 62 ms (34.95%), Space: 14.3 MB (10.63%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,17 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         codes = set(str(c) for c in range(1,27))
-        # print(codes)
+        def check(c):
+            if c[0] == "0" or not (1 <= int(c) <= 26 ): return False
+            return True
+        
+        # print(check("26"))
         @lru_cache(None)
         def dfs(i):
             if i >= len(s):
                 return 1
             
             res = 0
-            if i < len(s)-1 and s[i:i+2] in codes:
+            if i < len(s)-1 and  check(s[i:i+2]):
                 res += dfs(i+2)
             
-            if s[i] in codes:
+            if check(s[i]):
                 res += dfs(i+1)
             return res
         
