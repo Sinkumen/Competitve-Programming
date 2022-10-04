5ccefc58af00eff2ecc828b3bc9af18266277017
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-10-04 11:55:51 +0300

    Time: 109 ms (88.89%), Space: 47.6 MB (55.56%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.ts
@@ -0,0 +1,31 @@
+function maximalRectangle(matrix) {
+    const streaks = [];
+    let max = 0;
+    
+    for (let i = 0; i < matrix.length; i++) {
+        streaks[i] = [];
+        
+        for (let j = 0; j < matrix[i].length; j++) {
+            streaks[i][j] = matrix[i][j] === '0' ? 0 : 1 + (i === 0 ? 0 : streaks[i - 1][j]);
+        }
+    }
+    
+    for (let i = 0; i < matrix.length; i++) {
+        const stack = [];
+        
+        for (let j = 0; j <= matrix[i].length; j++) {
+            
+            while (stack.length > 0 && (j === matrix[i].length || streaks[i][j] <= streaks[i][stack.at(-1)])) {
+                const height = streaks[i][stack.pop()];
+                const width = stack.length === 0 ? j : j - stack.at(-1) - 1;
+                      
+                max = Math.max(max, width * height);
+            }
+            
+            stack.push(j);
+            
+        }
+    }
+    
+    return max;
+}
