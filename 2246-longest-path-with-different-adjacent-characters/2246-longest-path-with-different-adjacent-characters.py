982cfe208448dc371ae839df827c471a2c9de723
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-11-27 16:22:24 +0300

    Time: 4331 ms (50.74%), Space: 153 MB (50.90%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -4,20 +4,19 @@ class Solution:
         for i in range(len(parent)):
             if parent[i] != -1:               
                 graph[parent[i]].append(i)
-        # print(graph)
         
         def dfs(node):
             cur = 1 
             glb = 1
             for nxt in graph[node]:
                 c,g = dfs(nxt)
-                # print(node,c,g,s[nxt])
                 if not c or s[node] != s[nxt]:
                     two = c + cur 
                     cur = max(cur,c+1)
                     glb = max(glb,g,two)
                 else:
                     glb = max(glb,g)
+                    
             return (cur,glb)
         return dfs(0)[1]
                 
