0cf6791708f9cf16461a33d67c5a89530b8f440b
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2022-11-28 16:52:31 +0300

    Time: 3047 ms (36.69%), Space: 48.4 MB (63.00%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,26 +1,25 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        graph  = defaultdict(list)
-        incoming = defaultdict(list)
-        for frm,to in connections:
-            graph[frm].append(to)
-            incoming[to].append(frm)
+        graph = defaultdict(set)
+        outgoing = set()
+        for node1, node2 in connections:
+            outgoing.add((node1, node2))
+            graph[node1].add(node2)
+            graph[node2].add(node1)
+
+        queue = deque([0])
+        visited = {0}
         ans = 0
-        queue = deque([(0,0)])
-        visited = set([0])
+
         while queue:
-            cur,direc = queue.popleft()
-            if direc:
-                ans += 1
-            for nxt in graph[cur]:
-                if nxt not in visited:
-                    visited.add(nxt)
-                    queue.append((nxt,-1))
-            for prev in incoming[cur]:
-                if prev not in visited:
-                    visited.add(prev)
-                    queue.append((prev,0))
-            
+            current = queue.popleft()
+            for neigbor in graph[current]:
+                if neigbor not in visited:
+                    if (current, neigbor) in outgoing:
+                        ans += 1
+                    visited.add(neigbor)
+                    queue.append(neigbor)
+
         return ans
-                
+
         
