5c1f160eee2dc0f0de3926275d670f581afbe810
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2023-02-15 11:26:55 +0300

    Time: 433 ms (47.90%), Space: 27.6 MB (93.39%) - LeetHub

---
+++ 1583-count-unhappy-friends/1583-count-unhappy-friends.py
@@ -0,0 +1,27 @@
+class Solution:
+    def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
+        mp = {}
+        for a,b in pairs:
+            mp[a] = b
+            mp[b] = a
+        def check(a,b):
+            for pr in preferences[a]:
+                if pr == b:
+                    break
+                else:
+                    i = -1
+                    j = -1
+                    for temp in preferences[pr]:
+                        if temp == a and j == -1:
+                            return False
+                        if temp == mp[pr]:
+                            j = mp[0]
+            return True
+        ans = 0
+        for f,s in pairs:
+            if not check(f,s):
+                ans += 1
+            if not check(s,f):
+                ans += 1
+        return ans
+                            
