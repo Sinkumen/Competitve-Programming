862a8b38699b67e633339814d8bad20ae849d503
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2023-02-15 23:22:15 +0300

    Time: 455 ms (34.18%), Space: 32.7 MB (22.79%) - LeetHub

--- 1583-count-unhappy-friends/1583-count-unhappy-friends.py
+++ 1583-count-unhappy-friends/1583-count-unhappy-friends.py
@@ -4,18 +4,18 @@ class Solution:
         for a,b in pairs:
             mp[a] = b
             mp[b] = a
-        visited = set()
+        pref = defaultdict(dict)
+        for i in range(len(preferences)):
+            for j in range(len(preferences[i])):
+                val = preferences[i][j]
+                pref[i][val] = j
         def check(a,b):
             for pr in preferences[a]:
                 if pr == b:
                     break
-                for temp in preferences[pr]:
-                    if temp == a:
-                        visited.add(a)
-                        visited.add(pr)
-                        return False
-                    if temp == mp[pr]:
-                        break
+                if pref[pr][a] < pref[pr][mp[pr]]:
+                    return False
+                
             return True
         ans = 0
         for f,s in pairs:
@@ -23,5 +23,5 @@ class Solution:
                 ans += 1
             if not check(s,f):
                 ans += 1
-        return len(visited)
+        return ans
                             
