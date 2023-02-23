bc18ba77a99b1238da60b9666f113e54f6dcbf56
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2023-02-23 10:45:10 +0300

    Time: 534 ms (46.86%), Space: 13.9 MB (97.62%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,12 +1,12 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        mem = {1:1}
         def getPower(n):
-            if n in mem:
-                return mem[n]
-            if n%2 == 0:
-                mem[n] = 1 + getPower(n // 2)
-            else:
-                mem[n] = 1 + getPower(n * 3 + 1)
-            return mem[n]
+            ans = 0
+            while n != 1:
+                if n%2 == 0:
+                    n = n // 2
+                else:
+                    n = n * 3 + 1
+                ans += 1
+            return ans
         return sorted(range(lo,hi+1),key=lambda x: getPower(x))[k-1]
