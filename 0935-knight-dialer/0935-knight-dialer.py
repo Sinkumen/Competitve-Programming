2bc807f824f6e1fe17fe7e42d40d7765933df1ab
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2023-02-27 17:55:38 +0300

    Time: 6088 ms (8.47%), Space: 21.2 MB (47.70%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,12 +1,30 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        G = {0:[4,6], 1:[6,8], 2:[7,9], 3:[4,8], 4:[0,3,9], 5:[], 6:[0,1,7], 7:[2,6], 8:[1,3], 9:[2,4]}
-        M = 10**9 + 7
-        @cache
-        def dfs(v, i):
-            if i == 1: return 1
+        mod = 10**9 + 7
+        poss = {
+            0:[4,6],
+            1:[6,8],
+            2:[7,9],
+            3:[4,8],
+            4:[0,3,9],
+            5:[],
+            6:[0,1,7],
+            7:[2,6],
+            8:[1,3],
+            9:[2,4]
+        }
+        ans = 0
+        @lru_cache()
+        def dfs(i,size):
+            nonlocal ans
+            if size == n:
+                return 1
             res = 0
-            for w in G[v]:
-                res = (res + dfs(w, i-1)) % M
-            return res
-        return sum(dfs(v,n) for v in range(10)) % M
+            for nxt in poss[i]:
+                res += dfs(nxt,size+1) % mod
+            return res % mod
+                
+        for i in range(10):
+            ans += dfs(i,1)
+        return ans % mod
+                
