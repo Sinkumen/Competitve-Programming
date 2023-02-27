2b2ef3ba1f961bc2866338d5eb5ba384fffcda74
Sinkumen <39281650+Sinkumen@users.noreply.github.com>
2023-02-27 20:09:24 +0300

    Time: 1131 ms (59.71%), Space: 16.1 MB (53.33%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,30 +1,20 @@
+from functools import cache
 class Solution:
     def knightDialer(self, n: int) -> int:
         mod = 10**9 + 7
-        poss = {
-            0:[4,6],
-            1:[6,8],
-            2:[7,9],
-            3:[4,8],
-            4:[0,3,9],
-            5:[],
-            6:[0,1,7],
-            7:[2,6],
-            8:[1,3],
-            9:[2,4]
-        }
-        ans = 0
-        @lru_cache()
-        def dfs(i,size):
-            nonlocal ans
-            if size == n:
-                return 1
-            res = 0
-            for nxt in poss[i]:
-                res += dfs(nxt,size+1) % mod
-            return res % mod
-                
+        poss = { 0:[4,6], 1:[6,8], 2:[7,9], 3:[4,8], 4:[0,3,9], 5:[], 6:[0,1,7], 7:[2,6], 8:[1,3], 9:[2,4]}
+        dp = [[0 for i in range(10)]for _ in range(n)]
         for i in range(10):
-            ans += dfs(i,1)
-        return ans % mod
+            dp[n-1][i] = 1
+
+        for j in range(n-2,-1,-1):
+            for k in range(9,-1,-1):
+                summ = 0
+                for h in poss[k]:
+                    summ = (summ + dp[j+1][h]) % mod
+                dp[j][k] = summ
+        # print(dp[n-2])
+        # print(dp[n-1])
+        return sum(dp[0]) % mod
+        
                 
